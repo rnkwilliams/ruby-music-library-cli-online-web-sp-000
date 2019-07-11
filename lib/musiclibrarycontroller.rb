0fa37e3dbca0ee49946150c9827710ede27362d0
Ranika Williams <rnkprice07@gmail.com>
2019-07-11 18:05:17 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -61,12 +61,14 @@ end
      puts "Please enter the name of a genre:"
      user_input = gets.strip
      if genre = Genre.find_by_name(user_input)
-       sorted_array = genre.songs.sort{|a,b| a.name <=> b.name}
-       sorted_array.each_with_index do |song, index|
+       genre_sorted_array = genre.songs.sort{|a,b| a.name <=> b.name}
+       genre_sorted_array.each_with_index do |song, index|
          puts "#{index+1}. #{song.name} - #{song.genre.name}"
          end
        end
     end
+
+    
 
 
 end
